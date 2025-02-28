a67892a7a42df1e4dad63dd808cddb11ee3e8b39
shubu <shubu01001@gmail.com>
2025-03-01 02:05:50 +0530

    2 more tasks

--- random practice/script.js
+++ random practice/script.js
@@ -31,7 +31,7 @@
 
 // task3: convert a given temperature from celsius to fahrenheit and vice VideoColorSpace.
 
-function convertIntoCelsius() {
+/* function convertIntoCelsius() {
     let input = prompt(`enter your temperature in Fahrenheit`);
     celsius = (((input - 32) * 5) / 9);
     if (celsius % 1 !== 0) {
@@ -54,4 +54,39 @@ function convertIntoFahrenheit() {
         console.log(`${fahrenheit} degree fahrenheit`);
 
     }
+}
+ */
+
+// task4: create a program that swaps two variables' values without using a temporary variable.
+
+/* let a = 4;
+let b = 5;
+
+a = a + b;
+b = a - b;
+a = - (b - a);
+ */
+
+// task5: Check if a given year is leap year
+
+/* function isLeapYear() {
+    input = prompt("enter any year")
+    input % 4 == 0 ? console.log("this is a leap year") : console.log("this isn't a leap year");
+
+} */
+
+// task6: write a program that converts a number into a string and vice versa
+
+function checkInput() {
+    input = prompt("enter anything")
+    input
+    if (typeof input == "String") {
+        console.log("this is string");
+
+    } else if (typeof input == Number) {
+        console.log("this is number");
+
+    }
+
+
 }
