3bf86437db4c6d29ddb362492f5bba287ad4cfaf
shubham <shubu01001@gmail.com>
2025-03-26 16:20:50 +0530

    something added

--- random_practice/functions_scope.js
+++ random_practice/functions_scope.js
@@ -3,16 +3,16 @@ const input1 = document.getElementById("fNum");
 const input2 = document.getElementById("sNum");
 const input3 = document.getElementById("tNum");
 const button = document.getElementById("submit");
-
 const resultHold = document.createElement("p")
+const form = document.getElementById("form")
 
 // const result = document.createTextNode("")
 // const result = Math.max(inputs);
 
-button.addEventListener("click", () => {
-    const inputs = [input1.value, input2.value, input3.value];
-    console.log(inputs);
-
-
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const inputs = parseInt([input1.value, input2.value, input3.value]);
+    const result = Math.max(inputs);
+    console.log(result);
 })
 
