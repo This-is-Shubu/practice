672b35eda9f9685f60b9423e547fa593cfe7b94c
shubham <shubu01001@gmail.com>
2025-03-27 10:51:00 +0530

    1 task completed

--- random_practice/functions_scope.js
+++ random_practice/functions_scope.js
@@ -1,18 +1,31 @@
 // task1: write a function that returns the maximum of three numbers
-const input1 = document.getElementById("fNum");
+/* const input1 = document.getElementById("fNum");
 const input2 = document.getElementById("sNum");
 const input3 = document.getElementById("tNum");
-const button = document.getElementById("submit");
 const resultHold = document.createElement("p")
 const form = document.getElementById("form")
 
-// const result = document.createTextNode("")
-// const result = Math.max(inputs);
+
+const inputs = [];
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const inputs = parseInt([input1.value, input2.value, input3.value]);
-    const result = Math.max(inputs);
-    console.log(result);
-})
+    /*  inputs.push(input1.value, input2.value, input3.value);
+     const data = inputs.map(e => parseInt(e));
+     const result = Math.max(...data);
+     console.log(result);
+     resultHold.innerText = `the max number is ${result}`;
+     resultHold.style.color = "white";
+     document.body.appendChild(resultHold); */
 
+// inputs.push(input1.value, input2.value, input3.value);
+// const data = inputs.map(e => parseInt(e));
+
+/*  const result = Math.max(input1.value, input2.value, input3.value);
+ console.log(result);
+ resultHold.innerText = `the max number is ${result}`;
+ resultHold.style.color = "white";
+ document.body.appendChild(resultHold); */
+/*/
+})
+*/
