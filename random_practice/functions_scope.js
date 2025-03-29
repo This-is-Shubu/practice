c58fc11054a1d10f79c5c7bd2fc1802828627ac9
shubham <shubu01001@gmail.com>
2025-03-29 17:01:21 +0530

    2nd task completed

--- random_practice/functions_scope.js
+++ random_practice/functions_scope.js
@@ -21,20 +21,20 @@ form.addEventListener("submit", (e) => {
 // task2: Implement a function that counts the number of vowels in a string.
 
 const input = document.getElementById("paraInput")
-const btn = document.getElementById("submit")
-let count = 0;
+const btn = document.getElementById("submit1")
+
 
 function counter() {
+    let count = 0;
+    console.log(input.value);
     for (i = 0; i < input.value.length; i++) {
+
         if (/[aeiou]/i.test(input.value[i])) {
             count++;
         }
     }
-    return console.log(count);
+    console.log(count);
 }
 
-btn.addEventListener("click", () => {
-    counter();
-    ;
-})
+btn.addEventListener("click", counter)
 
