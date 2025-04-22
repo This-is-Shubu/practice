0e60d5f6a96aa52d19a745289cda0ef988a1e050
shubu <shubu01001@gmail.com>
2025-04-22 05:53:52 +0530

    rectified

--- random_practice/functions_scope.js
+++ random_practice/functions_scope.js
@@ -1,9 +1,9 @@
 // task1: write a function that returns the maximum of three numbers
-const input1 = document.getElementById("fNum");
-const input2 = document.getElementById("sNum");
-const input3 = document.getElementById("tNum");
-const resultHold = document.getElementById("p")
-const form = document.getElementById("form")
+const input1_1 = document.getElementById("fNum1");
+const input1_2 = document.getElementById("sNum1");
+const input1_3 = document.getElementById("tNum1");
+const resultHold = document.getElementById("p1")
+const form = document.getElementById("form1")
 
 
 const inputs = [];
@@ -11,18 +11,17 @@ const inputs = [];
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const result = Math.max(input1.value, input2.value, input3.value);
+    const result = Math.max(input1_1.value, input1_2.value, input1_3.value);
     console.log(result);
-    resultHold.querySelector("#result").textContent = result;
-
-
-
+    resultHold.querySelector("#result1").textContent = result;
 })
+
+
 // task2: Implement a function that counts the number of vowels in a string.
 
-const input = document.getElementById("paraInput")
-const btn = document.getElementById("submit1")
-const print = document.getElementById("p1")
+const input = document.getElementById("textArea2")
+const btn2 = document.getElementById("submit2")
+const print = document.getElementById("p2")
 
 
 function counter() {
@@ -34,10 +33,27 @@ function counter() {
         }
     }
     console.log(count);
-    print.querySelector("#result1").textContent = count;
+    print.querySelector("#result2").textContent = count;
 }
 
-btn.addEventListener("click", () => {
+btn2.addEventListener("click", () => {
     counter();
 })
+
+// task3: Create a function that takes a number and returns true if it's a prime number.
+
+const input3 = document.getElementById("input3")
+const btn3 =
+
+    function isPrime() {
+        if (input3 <= 1) return false;
+        if (input3 <= 3) return true;
+
+        if (input3 % 2 === 0 || input3 % 3 === 0) return false;
+
+        for (let i = 5; i * i <= input3; i += 6) {
+            if (input3 % i === 0 || input3 % (i + 2) === 0) return false;
+        }
+        return true;
+    }
 
